4c7aaf723c875542ac6c7b40d3d8d61dcdff39a7
Casey Vaughn <casey.vaughn9@aol.com>
2024-12-01 10:46:59 -0600

    Rename util variables

--- src/utils/utils.rs
+++ src/utils/utils.rs
@@ -2,8 +2,8 @@ use std::fs;
 use std::path::Path;
 
 pub fn read_file_data(relative_path: String) -> String {
-    let test_path = Path::new(&relative_path);
-    let test_data = fs::read_to_string(test_path).unwrap();
+    let path = Path::new(&relative_path);
+    let data = fs::read_to_string(path).unwrap();
 
-    test_data
+    data
 }
